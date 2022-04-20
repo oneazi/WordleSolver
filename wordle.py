bd1a34c0e594e2c300bbcb94fb66d5dae8c3d6dc
oneazi <omairneazi2000@gmail.com>
2022-04-19 18:53:27 -0700

    Grab basic elements from page

--- wordle.py
+++ wordle.py
@@ -16,5 +16,21 @@ browser.implicitly_wait(5)
 
 browser.get('https://www.nytimes.com/games/wordle/index.html')
 browser.find_element(by=By.XPATH, value="//body").click()
+
+# how to provide input to the game
 keyboard.write(choice(word_list), 0.05)
 keyboard.press_and_release('enter')
+
+# get each row after it is evaluated
+game_app = browser.find_element(By.TAG_NAME, 'game-app')
+board = browser.execute_script("return arguments[0].shadowRoot.getElementById('board')", game_app)
+game_rows = board.find_elements(By.TAG_NAME, 'game-row')
+game_row = game_rows[0]
+
+# execute this after each consecutive new guess
+row = browser.execute_script('return arguments[0].shadowRoot', game_row)
+tiles = row.find_elements(By.CSS_SELECTOR, "game-tile")
+for tile in tiles:
+    print(tile.get_attribute("evaluation"))
+print(game_rows)
+
